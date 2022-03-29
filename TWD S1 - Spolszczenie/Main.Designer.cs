74232c67727e5452f46dc90b6fd022b06ebdc5a9
fl-wer <101416707+fl-wer@users.noreply.github.com>
2022-03-29 01:38:39 +0100

    Add files via upload

---
+++ TWD S1 - Spolszczenie/Main.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace TWD_S1___Spolszczenie
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.patchGame = new System.Windows.Forms.Button();
+            this.chooseDirectory = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // patchGame
+            // 
+            this.patchGame.Enabled = false;
+            this.patchGame.Location = new System.Drawing.Point(12, 54);
+            this.patchGame.Name = "patchGame";
+            this.patchGame.Size = new System.Drawing.Size(326, 36);
+            this.patchGame.TabIndex = 2;
+            this.patchGame.Text = "instaluj";
+            this.patchGame.UseVisualStyleBackColor = true;
+            this.patchGame.Click += new System.EventHandler(this.patchGame_Click);
+            // 
+            // chooseDirectory
+            // 
+            this.chooseDirectory.Location = new System.Drawing.Point(12, 12);
+            this.chooseDirectory.Name = "chooseDirectory";
+            this.chooseDirectory.Size = new System.Drawing.Size(326, 36);
+            this.chooseDirectory.TabIndex = 4;
+            this.chooseDirectory.Text = "wybierz folder z grą";
+            this.chooseDirectory.UseVisualStyleBackColor = true;
+            this.chooseDirectory.Click += new System.EventHandler(this.chooseDirectory_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 99);
+            this.Controls.Add(this.chooseDirectory);
+            this.Controls.Add(this.patchGame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TWD S1 - Spolszczenie";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowser;
+        private System.Windows.Forms.Button patchGame;
+        private System.Windows.Forms.Button chooseDirectory;
+    }
+}
+
---
+++ TWD S1 - Spolszczenie/Main.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TWD_S1___Spolszczenie
+{
+    public partial class Main : Form
+    {
+        // initializing things, ignore this
+        public Main() { InitializeComponent(); }
+
+        // this runs when you start program, nothing here so ignore
+        private void Main_Load(object sender, EventArgs e) { }
+
+        // choosing game directory, has to be exact game path, not folder inside
+        private void chooseDirectory_Click(object sender, EventArgs e)
+        {
+            // show dialogue with folder selection
+            folderBrowser.ShowDialog();
+
+            // making sure all folders exist so writing files won't fail
+            if (Directory.Exists(folderBrowser.SelectedPath + "\\" + "Pack\\default") &&
+            Directory.Exists(folderBrowser.SelectedPath + "\\" + "Pack\\WalkingDead102") &&
+            Directory.Exists(folderBrowser.SelectedPath + "\\" + "Pack\\WalkingDead103") &&
+            Directory.Exists(folderBrowser.SelectedPath + "\\" + "Pack\\WalkingDead104") &&
+            Directory.Exists(folderBrowser.SelectedPath + "\\" + "Pack\\WalkingDead105"))
+            {
+                // all folders found
+                showInfo("Lokalizacja poprawna.");
+                patchGame.Enabled = true;
+
+            }
+            else
+            {
+                // some folders were not found
+                showError("Lokalizacja niepoprawna.");
+                patchGame.Enabled = false;
+            }
+        }
+
+        // writes language patching files into game folder
+        private void patchGame_Click(object sender, EventArgs e)
+        {
+            // putting game + pack path to 1 variable for readability
+            string epPath = folderBrowser.SelectedPath + "\\Pack\\";
+
+            // putting in try as this can throw exception in some cases
+            try 
+            {
+                File.WriteAllBytes(epPath + "\\default\\0.ttarch", Properties.Resources.ep1);
+                File.WriteAllBytes(epPath + "\\WalkingDead102\\0.ttarch", Properties.Resources.ep2);
+                File.WriteAllBytes(epPath + "\\WalkingDead103\\0.ttarch", Properties.Resources.ep3);
+                File.WriteAllBytes(epPath + "\\WalkingDead104\\0.ttarch", Properties.Resources.ep4);
+                File.WriteAllBytes(epPath + "\\WalkingDead105\\0.ttarch", Properties.Resources.ep5);
+
+                // success
+                showInfo("patchowanie zakończone sukcesem, miłej gry");
+            }
+            // something failed, maybe file already there and in use
+            catch { showError("błąd podczas patchowania"); }
+        }
+
+        // show custom message box with warning or error icon
+        public static void showError(string message) { MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        public static void showInfo(string message) { MessageBox.Show(message, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information); }
+    }
+}
